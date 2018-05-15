0d9ce8df681debbcd47dd8a72cdc323f35b515de
Jon Calhoun <joncalhoun@gmail.com>
2018-05-15 18:08:26 -0400

    ignoring secrets

--- cmd/demo.go
+++ cmd/demo.go
@@ -7,12 +7,30 @@ import (
 )
 
 func main() {
-	v := secret.Memory("my-fake-key")
-	err := v.Set("demo_key", "some crazy value")
+	v := secret.File("my-fake-key", ".secrets")
+	err := v.Set("demo_key1", "123 some crazy value")
 	if err != nil {
 		panic(err)
 	}
-	plain, err := v.Get("demo_key")
+	err = v.Set("demo_key2", "456 some crazy value")
+	if err != nil {
+		panic(err)
+	}
+	err = v.Set("demo_key3", "789 some crazy value")
+	if err != nil {
+		panic(err)
+	}
+	plain, err := v.Get("demo_key1")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Plain:", plain)
+	plain, err = v.Get("demo_key2")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Plain:", plain)
+	plain, err = v.Get("demo_key3")
 	if err != nil {
 		panic(err)
 	}
--- vault.go
+++ vault.go
@@ -1,40 +1,101 @@
 package secret
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"sync"
 
 	"github.com/gophercises/secret/encrypt"
 )
 
-func Memory(encodingKey string) Vault {
-	return Vault{
+func File(encodingKey, filepath string) *Vault {
+	return &Vault{
 		encodingKey: encodingKey,
-		keyValues:   make(map[string]string),
+		filepath:    filepath,
 	}
 }
 
 type Vault struct {
 	encodingKey string
+	filepath    string
+	mutex       sync.Mutex
 	keyValues   map[string]string
 }
 
-func (v *Vault) Get(key string) (string, error) {
-	hex, ok := v.keyValues[key]
-	if !ok {
-		return "", errors.New("secret: no value for that key")
-	}
-	ret, err := encrypt.Decrypt(v.encodingKey, hex)
+func (v *Vault) loadKeyValues() error {
+	f, err := os.Open(v.filepath)
 	if err != nil {
-		return "", err
+		v.keyValues = make(map[string]string)
+		return nil
 	}
-	return ret, nil
-}
-
-func (v *Vault) Set(key, value string) error {
-	encryptedValue, err := encrypt.Encrypt(v.encodingKey, value)
+	defer f.Close()
+	var sb strings.Builder
+	_, err = io.Copy(&sb, f)
 	if err != nil {
 		return err
 	}
-	v.keyValues[key] = encryptedValue
+	decryptedJSON, err := encrypt.Decrypt(v.encodingKey, sb.String())
+	if err != nil {
+		return err
+	}
+	r := strings.NewReader(decryptedJSON)
+	dec := json.NewDecoder(r)
+	err = dec.Decode(&v.keyValues)
+	if err != nil {
+		return err
+	}
 	return nil
+}
+
+func (v *Vault) saveKeyValues() error {
+	var sb strings.Builder
+	enc := json.NewEncoder(&sb)
+	err := enc.Encode(v.keyValues)
+	if err != nil {
+		return err
+	}
+	encryptedJSON, err := encrypt.Encrypt(v.encodingKey, sb.String())
+	if err != nil {
+		return err
+	}
+	f, err := os.OpenFile(v.filepath, os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	_, err = fmt.Fprint(f, encryptedJSON)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (v *Vault) Get(key string) (string, error) {
+	v.mutex.Lock()
+	defer v.mutex.Unlock()
+	err := v.loadKeyValues()
+	if err != nil {
+		return "", err
+	}
+	value, ok := v.keyValues[key]
+	if !ok {
+		return "", errors.New("secret: no value for that key")
+	}
+	return value, nil
+}
+
+func (v *Vault) Set(key, value string) error {
+	v.mutex.Lock()
+	defer v.mutex.Unlock()
+	err := v.loadKeyValues()
+	if err != nil {
+		return err
+	}
+	v.keyValues[key] = value
+	err = v.saveKeyValues()
+	return err
 }
